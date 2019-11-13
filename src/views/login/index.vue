c0055acae3ef78977b462c7f093876e7f3f0df5d
阿龙 <along@alongdeMacBook-Pro.local>
2019-11-13 11:20:33 +0800

    登陆模块-登陆自动验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,17 +6,22 @@
         <div class="title">
             <img src="../../assets/img/logo_index.png" alt="">
         </div>
-        <el-form style="margin-top:30px">
-            <el-form-item>
-                <el-input></el-input>
+        <!-- 表单                -----------------            验证---------- -->
+        <el-form style="margin-top:30px" :model='loginForm' :rules='loginRules'>
+            <el-form-item prop="mobile">
+                <!-- 手机号 -->
+                <el-input v-model="loginForm.mobile" placeholder="请输入您的手机号"></el-input>
             </el-form-item>
-            <el-form-item>
-                <el-input style="width:280px"></el-input>
-                <el-button style='float:right' plain>朴素按钮</el-button>
+
+            <el-form-item prop="code">
+                <!-- 验证码 -->
+                <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:280px"></el-input>
+                <el-button style='float:right' plain>发送验证码</el-button>
             </el-form-item>
-            <el-form-item>
+
+            <el-form-item prop="checked">
                 <!-- 勾选框 -->
-                <el-checkbox>我已阅读并同意用户协议及条款</el-checkbox>
+                <el-checkbox v-model="loginForm.checked">我已阅读并同意用户协议及条款</el-checkbox>
 
             </el-form-item>
             <el-form-item>
@@ -31,7 +36,48 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // 要校验的表单数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 勾选框
 
+      },
+      // 校验规则对象
+      loginRules: {
+        // key(要校验的字段名):value(数组：多条或1条或没有s)
+
+        // 手机号验证
+        mobile: [
+          { required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入正确手机号' }
+        ],
+        // 验证码
+        code: [
+          { required: true, message: '请输入你的验证码' },
+          { pattern: /^\d{6}$/, message: '请输入正确验证码' }
+        ],
+        // 勾选框
+        checked: [
+          // 自定义校验
+          { validator: function (rule, value, callback) {
+            //  rule     代表当潜规则
+            //  value    代表当前值
+            //  callback 回调函数
+            if (value) {
+              // true 代表通过
+              callback()
+            } else {
+            //   false代表不通过
+              callback(new Error('您需要勾选用户协议'))
+            }
+          } }
+        ]
+      }
+    }
+  }
 }
 </script>
 
