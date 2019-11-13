2e9fd6846114b4b5f16cee20862170010ec4ca34
阿龙 <along@alongdeMacBook-Pro.local>
2019-11-13 09:22:55 +0800

    初始化项目-页面分析-新建登陆和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home' // 简写路径
+import Login from '../views/login' // 登陆
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/home'
+  },
+  // 一级路由  主页
+  {
+    path: '/home',
     name: 'home',
     component: Home
+  },
+  // 一级路由  登陆
+  {
+    path: '/login',
+    component: Login
   }
   // {
   //   path: '/about',
