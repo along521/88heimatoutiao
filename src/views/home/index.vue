3548b2000524a3b4443aeaa173a4da904379348f
阿龙 <along@alongdeMacBook-Pro.local>
2019-11-13 14:51:11 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>主页</div>
+  <!-- 布局 -->
+  <el-container>
+  <!-- 左右 左侧栏 -->
+  <el-aside>左侧内容</el-aside>
+
+  <!-- 大容器 -->
+  <el-container>
+    <!-- 上 头部 -->
+    <el-header>头部</el-header>
+
+    <!-- 下 中间-->
+    <el-main>
+      <!-- 二级路由容器 -->
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+
+  </el-container>
 </template>
 
 <script>
